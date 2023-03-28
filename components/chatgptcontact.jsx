76e72e68cd6b50c819545a128e9f625925607a18
YasteneAK <121824763+YasteneAK@users.noreply.github.com>
2023-03-28 23:00:00 +0200

    Add files via upload

---
+++ components/chatgptcontact.jsx
@@ -0,0 +1,58 @@
+import { useState } from 'react'
+
+function ServicesPage() {
+  return (
+    <div>
+      <h1>Our Services</h1>
+      <p>We offer a wide range of services to help your business succeed:</p>
+      <ul>
+        <li>
+          <h2>Web and Mobile Development</h2>
+          <p>
+            Our team of experienced developers can build custom websites and mobile apps to meet the unique needs of your business. We use the latest technologies and frameworks to deliver high-quality, scalable solutions.
+          </p>
+          <img src="/images/web-mobile-development.png" alt="Web and Mobile Development" />
+        </li>
+        <li>
+          <h2>Coding</h2>
+          <p>
+            From custom software development to automation and data analysis, we can help you tackle any coding challenge. Our team has expertise in a variety of programming languages, including Python, JavaScript, and Java.
+          </p>
+          <img src="/images/coding.png" alt="Coding" />
+        </li>
+        <li>
+          <h2>Data Science and Analysis</h2>
+          <p>
+            Leverage the power of data to drive business growth and make informed decisions. Our data scientists and analysts can help you collect, clean, and analyze data, and visualize insights using tools like R and Python.
+          </p>
+          <img src="/images/data-science-analysis.png" alt="Data Science and Analysis" />
+        </li>
+        <li>
+          <h2>Digital Marketing</h2>
+          <p>
+            Get the word out about your business with our digital marketing services. We offer a range of services, including social media marketing, email marketing, and search engine marketing, to help you reach and engage with your target audience.
+          </p>
+          <img src="/images/digital-marketing.png" alt="Digital Marketing" />
+        </li>
+        <li>
+          <h2>SEO Referencing</h2>
+          <p>
+            Improve the visibility of your website in search engine results pages with our SEO services. We can help you optimize your website for relevant keywords, improve the user experience, and build high-quality backlinks.
+          </p>
+          <img src="/images/seo-referencing.png" alt="SEO Referencing" />
+        </li>
+        <li>
+          <h2>IT Consulting</h2>
+          <p>
+            Our IT consultants can help you assess your technology needs and develop a roadmap for success. We can provide guidance on topics such as cloud computing, cybersecurity, and data management.
+          </p>
+          <img src="/images/it-consulting.png" alt="IT Consulting" />
+        </li>
+        <li>
+          <h2>IT Support and Administration</h2>
+          <p>
+            Keep your business running smoothly with our IT support and administration services. We can help you troubleshoot technical issues, manage your network and servers, and provide ongoing maintenance and support.
+            </p>
+            </li>
+            </ul>
+            </div>
